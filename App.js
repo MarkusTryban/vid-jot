53bd0c938bf394bce40a5e64dff573acb9f8052a
MarkusTryban <markustryban@gmail.com>
2019-11-20 21:10:59 -0500

    Fix typo

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 const express = require('express');
 
-const App = express();
+const app = express();
 
 // Index route
-App.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('INDEX');
 });
 
 // About route
-App.get('/about', (req, res) => {
+app.get('/about', (req, res) => {
   res.send('ABOUT');
 });
 
 const port = 5000;
 
-App.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
